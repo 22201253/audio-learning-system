8521944a74e5cfd6e2a8b0e11a89dbd6bf0c4bf5
Seth <okabisunday@gmail.com>
2026-02-10 00:14:35 +0200

    Fix Reflex admin config for Render

--- admin-dashboard/rxconfig.py
+++ admin-dashboard/rxconfig.py
@@ -2,6 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="admin_dashboard",
-    api_url="https://audio-learning-system.onrender.com",  # We'll change this later
+    deploy_url="https://audio-learning-admin.onrender.com",
     state_auto_setters=True,
 )
