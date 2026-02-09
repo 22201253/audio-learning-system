081ecd1e6e30540c295cef09495c9a4e06f2c501
Seth <okabisunday@gmail.com>
2026-02-09 20:50:15 +0200

    Update rxconfig.py for production

--- admin-dashboard/rxconfig.py
+++ admin-dashboard/rxconfig.py
@@ -2,6 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="admin_dashboard",
-    api_url="BACKEND_URL_HERE",  # We'll change this later
+    api_url="https://audio-learning-system.onrender.com",  # We'll change this later
     state_auto_setters=True,
 )
